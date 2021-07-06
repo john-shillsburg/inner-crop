71c5cf70e7148e22e55fcc698f066302331ddc2d
Jordan Camp <jordan.camp748@gmail.com>
2021-07-06 10:45:08 -0500

    added slope function

--- inner-crop.rb
+++ inner-crop.rb
@@ -1,8 +1,13 @@
 require 'rubygems'
 require 'rmagick'
+require_relative 'src/utility.rb'
+
 img = Magick::Image::read("test/satellite.jpg")[0]
 
-img.scale(0.2).write("test/scaled.jpg") do |f|
+img.scale(0.1).write("test/scaled.jpg") do |f|
   f.quality = 0.6
 end
 
+img = Magick::Image::read("test/scaled.jpg")[0]
+
+get_slope(img)
--- src/utility.rb
+++ src/utility.rb
@@ -1,1 +1,10 @@
 # this file contains various utility function for working with Pixel Objects
+# and Image objects in the rmagick library
+
+# take an image and get the slope of the top line of the inner rectangle
+def get_slope(img)
+  img.each_pixel do |pixel, col, row|
+    puts "Pixel at: #{col}x#{row}:
+  \tR: #{pixel.red}, G: #{pixel.green}, B: #{pixel.blue}"
+  end
+end
