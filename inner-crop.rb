5a16e529b30d29a7410258f5c96162da092b867d
Jordan Camp <jordan.camp748@gmail.com>
2021-07-18 20:55:49 -0500

    rotated

--- inner-crop.rb
+++ inner-crop.rb
@@ -11,4 +11,5 @@ end
 img = Magick::Image::read("test/scaled.jpg")[0]
 
 sat = SatelliteImage.new(img)
+sat.rotate()
 
--- src/satellite_image.rb
+++ src/satellite_image.rb
@@ -6,12 +6,9 @@ class SatelliteImage
   # take an image and get the slope of the top line of the inner rectangle
   def initialize(img)
     @img = img
-    @width
-    @height
     @top_left = Array.new
     @top_right = Array.new
-    @bottom_left = Array.new
-    @bottom_right = Array.new
+    @angle
 
     @img.each_pixel do |pixel, col, row|
       if pixel.red > 4000 && pixel.green > 4000 && pixel.blue > 4000
@@ -20,7 +17,24 @@ class SatelliteImage
         break
       end
     end
-    puts @top_left
+
+    @img.each_pixel do |pixel, col, row|
+      if col == @img.columns - 2
+        if pixel.red > 4000 && pixel.green > 4000 && pixel.blue > 4000
+          @top_right.push(col)
+          @top_right.push(row)
+          break
+        end
+      end
+    end
+    pry
+    @angle = Math.atan(Float(@top_right[1]) / (Float(@img.columns) - Float(@top_left[0])))
+    @angle = @angle * 180 / Math::PI
+  end
+
+  def rotate()
+    rotated = @img.rotate(-@angle)
+    rotated.write("rotated.jpg")
   end
 end
 
